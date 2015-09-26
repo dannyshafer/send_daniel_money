e8690215e43b90cbd1eba8bbe14ca5ea7cf0030c
dannyshafer <github@danielshafer.com>
2015-09-26 15:28:48 -0700

    add line to logged in page, change default route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/loggedin'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root 'setup#index'
+  root 'welcome#index'
   get '/setup' => 'setup#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
